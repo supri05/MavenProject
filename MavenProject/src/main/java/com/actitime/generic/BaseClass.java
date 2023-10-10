93f923d001f8bec354a64ce57bd13f6db5ffa507
dell <dell@DESKTOP-3N0O4DU>
2023-10-10 17:26:34 +0530

    First COMMIT

--- MavenProject/src/main/java/com/actitime/generic/BaseClass.java
+++ MavenProject/src/main/java/com/actitime/generic/BaseClass.java
@@ -1,44 +1,44 @@
-package com.actitime.generic;
-
-import org.openqa.selenium.WebDriver;
-
-public class BaseClass {
-	public  WebDriver driver;
-//	@BeforeClass
-////	@Parameters("browser")
-////	public void openBrowser() throws IOException {
-////		driver=new ChromeDriver();
-////		driver.manage().window().maximize();
-////		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-////		FileLib f = new FileLib();
-////		String s = f.readDataFromProperty("url");
-////		driver.get(s);
-////		Reporter.log("openBrowser", true);
+//package com.actitime.generic;
+//
+//import org.openqa.selenium.WebDriver;
+//
+//public class BaseClass {
+//	public  WebDriver driver;
+////	@BeforeClass
+//////	@Parameters("browser")
+//////	public void openBrowser() throws IOException {
+//////		driver=new ChromeDriver();
+//////		driver.manage().window().maximize();
+//////		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+//////		FileLib f = new FileLib();
+//////		String s = f.readDataFromProperty("url");
+//////		driver.get(s);
+//////		Reporter.log("openBrowser", true);
+//////	}
+////
+////	@BeforeMethod
+//////	public void login() throws EncryptedDocumentException, IOException {
+//////		LoginPage lg = new LoginPage(driver);
+//////		FileLib f = new FileLib();
+//////		String us = f.readDataFromProperty("username");
+//////		String pw = f.readDataFromProperty("password");
+//////        lg.login(us, pw);
+//////		Reporter.log("login", true);
 ////	}
-//
-//	@BeforeMethod
-////	public void login() throws EncryptedDocumentException, IOException {
-////		LoginPage lg = new LoginPage(driver);
-////		FileLib f = new FileLib();
-////		String us = f.readDataFromProperty("username");
-////		String pw = f.readDataFromProperty("password");
-////        lg.login(us, pw);
-////		Reporter.log("login", true);
-//	}
-//
-//	@AfterMethod
-////	public void logout() {
-////		HomePage hg = new HomePage(driver);
-////		hg.logout();
-////		
-////		Reporter.log("logout", true);
+////
+////	@AfterMethod
+//////	public void logout() {
+//////		HomePage hg = new HomePage(driver);
+//////		hg.logout();
+//////		
+//////		Reporter.log("logout", true);
+//////	}
+////
+////	@AfterClass
+////	public void closedBrowser() {
+////		driver.quit();
+////		Reporter.log("closedBrowser", true);
 ////	}
-//
-//	@AfterClass
-//	public void closedBrowser() {
-//		driver.quit();
-//		Reporter.log("closedBrowser", true);
-//	}
-//
-//	
-}
+////
+////	
+//}
